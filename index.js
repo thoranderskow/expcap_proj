a740e1682a8f15bf728f387a97efb784b7a5f50b
thoranderskow <tanderskow@gmail.com>
2021-03-15 03:27:37 -0500

    initialized

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+    });
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+});
+
+http.listen(process.env.PORT || 3000, () => {
+	console.log('listening on *:3000');
+    });
