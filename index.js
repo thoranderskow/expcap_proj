58f8ca43d4346a349c61ca864b0c836cd8cdadc2
thoranderskow <tanderskow@gmail.com>
2021-03-17 21:05:48 -0500

    done for now

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
     });
 
+function get_random_color() {
+	function c() {
+		var hex = Math.floor(Math.random()*256).toString(16);
+		return ("0"+String(hex)).substr(-2); // pad with zero
+	}
+	return "#"+c()+c()+c();
+}
+
 io.on('connection', (socket) => {
+	socket.on('newplayer', (msg) => {
+		socket.emit('size', [Math.floor(Math.random() * 20) + 1, get_random_color()]);
+	});
+
 	console.log('a user connected');
 	socket.on('drawn', (msg) => {
 		socket.broadcast.emit('updateall', msg);
@@ -14,6 +26,6 @@ io.on('connection', (socket) => {
 });
 
 
-http.listen(process.env.PORT || 8000, () => {
-	console.log('listening on *:8000');
+http.listen(process.env.PORT || 7000, () => {
+	console.log('listening on *:7000');
     });
